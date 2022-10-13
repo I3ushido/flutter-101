cd1ad7746ddeaf1afd76c6313a17408cc81fc835
Bushido <phisanurat.won@msu.ac.th>
2022-10-13 15:12:20 +0700

    update packages flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:myapp/pages/home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
       title: 'My App',
     );
