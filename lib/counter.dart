72cfce3c2ceb380213a3d89a719f1379e4091cdf
Bushido <phisanurat.won@msu.ac.th>
2022-10-26 21:26:45 +0700

    add unit test for counter-class

---
+++ lib/counter.dart
@@ -0,0 +1,10 @@
+// https://docs.flutter.dev/cookbook/testing/unit/introduction
+// Unit tests.
+
+class Counter {
+  int value = 0;
+
+  void increments() => value += 1;
+
+  void decrements() => value -= 1;
+}
---
+++ test/counter_test.dart
@@ -0,0 +1,22 @@
+import 'package:myapp/counter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+      counter.increments();
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+      counter.decrements();
+      expect(counter.value, -1);
+    });
+  });
+}
